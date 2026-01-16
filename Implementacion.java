dfb4ad92eca07e73666e0647f30e8f7456663cce
garzzaro <barbaragarzaro06@gmail.com>
2026-01-16 09:43:43 -0600

    private

--- Implementacion.java
+++ Implementacion.java
@@ -1,3 +1,12 @@
 public class Implementacion {
+    public private boolean apagarRadio;
+    public private boolean avanzarEstacion;
+    public private boolean guardarEstacion;
+    public private  boolean cargarEstacion;
+    public private boolean cambiarFM; 
+    public private boolean cambiarAM;
+
+
+
     
 }
